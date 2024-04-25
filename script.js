d119ae3dd7bce21522da344e411bdf5e42e70bdb
ashxsong <beethoven6198@gmail.com>
2024-04-25 15:45:40 -0500

    Create function to play game
    Added function to play Rock Paper Scissors five times with scores

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ function getHumanChoice() {
   return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice) {
   let capitalHumanChoice = humanChoice[0].toUpperCase() + humanChoice.substr(1, humanChoice.length);
   let capitalComputerChoice = computerChoice[0].toUpperCase() + computerChoice.substr(1, computerChoice.length);
@@ -24,18 +21,44 @@ function playRound(humanChoice, computerChoice) {
       (humanChoice == "paper" && computerChoice == "rock") ||
       (humanChoice == "scissors" && computerChoice == "paper")) {
     console.log(`You win! ${capitalHumanChoice} beats ${capitalComputerChoice}`);
-    humanScore++;
+    return "human";
   }
   else if (humanChoice == computerChoice) {
     console.log(`It's a tie with ${capitalHumanChoice}!`);
+    return "none";
   }
   else {
     console.log(`You lose! ${capitalComputerChoice} beats ${capitalHumanChoice}`);
-    computerScore++;
+    return "computer";
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    let round = playRound(humanSelection, computerSelection);
+    if (round == "human") {
+      humanScore++;
+    }
+    if (round == "computer") {
+      computerScore++;
+    }
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+    console.log("");
+  }
+  if (humanScore < computerScore) {
+    console.log("You lose!");
+  }
+  else if (humanScore > computerScore) {
+    console.log("You win!");
+  }
+  else {
+    console.log("It's a tie!");
+  }
+}
 
-playRound(humanSelection, computerSelection);
+playGame();
