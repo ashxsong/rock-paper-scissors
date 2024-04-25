61f74a5e5979eb30302d877c8be07de79e7b9e44
ashxsong <beethoven6198@gmail.com>
2024-04-25 15:20:13 -0500

    Create function to play one round
    Added function to play one round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function getHumanChoice() {
   }
   return choice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  let capitalHumanChoice = humanChoice[0].toUpperCase() + humanChoice.substr(1, humanChoice.length);
+  let capitalComputerChoice = computerChoice[0].toUpperCase() + computerChoice.substr(1, computerChoice.length);
+  if ((humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "scissors" && computerChoice == "paper")) {
+    console.log(`You win! ${capitalHumanChoice} beats ${capitalComputerChoice}`);
+    humanScore++;
+  }
+  else if (humanChoice == computerChoice) {
+    console.log(`It's a tie with ${capitalHumanChoice}!`);
+  }
+  else {
+    console.log(`You lose! ${capitalComputerChoice} beats ${capitalHumanChoice}`);
+    computerScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
